7fb0a71133bef8664e5e7b2cffdee064e769d332
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2024-07-12 00:03:51 -0300

    Configure tailwind.config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'criancavrcortada': "url('../assets/png/criancavrcortada.png')",
+        'criancavr': "url('../assets/png/criancavr.png')",
+        'square': "url('../assets/svg/square.svg')",
+      },
       colors: {
         antonBackgroundcolor: "#CFCFCF",
         antonGrey: "#585858",
@@ -10,13 +16,20 @@ module.exports = {
         antonTextWhite: "#E5F5F4",
         antonTextBlack: "#111111",
         antonOrange: "#FF5C00",
-        antonSalmon:{
-          darksalmon:"#FF6554",
-          lightsalmon:"#FFB2A9",
+        antonSalmon: {
+          darksalmon: "#FF6554",
+          lightsalmon: "#FFB2A9",
         },
+      },
+      fontFamily: {
+        'audio': ['var(--audiowide)', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      animation: {
+        'bounce-slow': 'bounce 2s cubic-bezier(0.5, 0, 1, 1) infinite',
+        'spin-slow': 'spin 11s linear infinite',
       },
     },
   },
   plugins: [],
 }
-
