cc54ff266f38fa71ca4902f3f32e3dd340afddfb
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2024-07-12 00:18:15 -0300

    Add ReactSVG with animation

---
+++ components/ReactSvg.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import ReactSymbol from "/assets/svg/REACTSYMBOL.svg";
+
+const ReactSvg = () => (
+    <div className="w-full h-full z-10">
+        <div className="absolute top-[700px] -left-[450px] lg:top-[1000px] md:-left-[600px] overflow-hidden ">
+            <div className="">
+                <Image
+                    src={ReactSymbol}
+                    className="animate-spin-slow"
+                    alt="Simbolo do Framework React"
+                    width={1200}
+                    height={1200}
+                />
+            </div>
+        </div>
+    </div>
+);
+export default ReactSvg;
